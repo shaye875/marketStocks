ad4281c6b43c9b86dfceb04a94fe8ddd5028dc03
shaye <sm0548540902@gmail.com>
2025-12-11 11:48:35 +0200

    add function top Decreasing

--- bonus/topstocks.js
+++ bonus/topstocks.js
@@ -21,6 +21,24 @@ export function topIncreasingStocks() {
     return top3
 }
 
-export function topDecreasingStocks(){
-    
+export function topDecreasingStocks() {
+    let top = stockMarket.stocks.filter((s) => {
+        if (s.previousPrices.length >= 1) {
+            return s
+        }
+    })
+    let topDec3 = []
+    top.forEach((s) => {
+        if (topDec3.length < 3) {
+            topDec3.push(s)
+        } else {
+            for (let s1 of topDec3) {
+                if (s.previousPrices[s.previousPrices.length - 1] - s1.previousPrices[0] < s1.previousPrices[s1.previousPrices.length - 1] - s1.previousPrices[0]) {
+                    topDec3.splice(topDec3.indexOf(s1), 1, s)
+                }
+            }
+        }
+    })
+    return topDec3
 }
+
